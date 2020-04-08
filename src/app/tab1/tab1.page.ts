76329322ed7c7d4d90d633fcc43e43a6e018b1fb
JAllen43 <60601777+JAllen43@users.noreply.github.com>
2020-04-08 15:48:27 +0100

    Home Screen working

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  inputtedUsername:string;
+  inputtedPassword:string;
   constructor() {}
+
+  onLogin(){
+
+
+  }
+
+  onRegister(){
+
+  }
 
 }
